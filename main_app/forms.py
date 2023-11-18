c606946eb89865ca3f17790be3ac9a554b65c50e
Jan Hatapka <jan.hatapka@gmail.com>
2023-11-18 21:22:39 +0100

    sign up added data, fixed issues

--- main_app/forms.py
+++ main_app/forms.py
@@ -10,41 +10,72 @@ class StudentSignupForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = User
+        fields = ('email', 'first_name', 'last_name', 'password1', 'password2')
 
-    @transaction.atomic
-    def save(self):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data.get('email')
-        user.is_student = True
-        user.save()
-        student = Student.objects.create(user=user)
-        return user
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already in use.")
+        return email
 
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
 
-class TeacherSignUpForm(forms.ModelForm):
-    email = forms.EmailField(required=True)
-    phone = forms.CharField(required=True)
-    destination = forms.CharField(required=True)
-    password1 = forms.CharField(widget=forms.PasswordInput, required=True)
-    password2 = forms.CharField(widget=forms.PasswordInput, required=True)
-
-    class Meta:
-        model = User
-        fields = ('email', 'phone', 'destination', 'password1', 'password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords do not match.")
 
     @transaction.atomic
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.username = self.cleaned_data.get('email')  # You may customize the username as per your requirements
+        user.username = self.cleaned_data.get('email')
+        user.is_student = True
+        user.set_password(self.cleaned_data.get('password1'))
+
+        if commit:
+            user.save()
+        first_name = self.cleaned_data.get("first_name")
+        last_name = self.cleaned_data.get("last_name")
+        student = Student.objects.create(user=user, first_name=first_name, last_name=last_name )
+        student.save()
+
+        return user
+
+
+class TeacherSignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already in use.")
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords do not match.")
+
+    @transaction.atomic
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.username = self.cleaned_data.get('email')
         user.is_teacher = True
         user.set_password(self.cleaned_data.get('password1'))
 
         if commit:
             user.save()
-
-        teacher = Teacher.objects.create(user=user)
-        teacher.phone = self.cleaned_data.get('phone')
-        teacher.destination = self.cleaned_data.get('destination')
+        first_name = self.cleaned_data.get("first_name")
+        last_name = self.cleaned_data.get("last_name")
+        teacher = Teacher.objects.create(user=user, first_name=first_name, last_name=last_name )
         teacher.save()
 
         return user
--- main_app/models.py
+++ main_app/models.py
@@ -12,11 +12,22 @@ class User(AbstractUser):
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    first_name = models.CharField(max_length=200, blank=True)
+    last_name = models.CharField(max_length=200, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    student_image = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 
 class Teacher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    phone = models.CharField(max_length=200)
-    destination = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 
 
